32b4701280291e56109f7f854f13a2b5b9190eca
Dan Burton <danburton720@hotmail.com>
2020-08-25 14:06:31 +0100

    added some more pages

---
+++ git/gitplayground/spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,64 @@
+package com.luv2code.springdemo.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+	// need a controller method to show the initial HTML form
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	
+	// need a controller method to process the HTML form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+	/*
+	 * // controller method to read form data and add data to the model
+	 * 
+	 * @RequestMapping("/processFormVersion2") public String
+	 * letsShoutDude(HttpServletRequest request, Model model) {
+	 * 
+	 * // read the request parameter from the HTML form String theName =
+	 * request.getParameter("studentName");
+	 * 
+	 * // convert the data to all upper case theName = theName.toUpperCase();
+	 * 
+	 * // create the message String result = "Yo " + theName + "!";
+	 * 
+	 * // add message to the model model.addAttribute("message", result);
+	 * 
+	 * return "helloworld";
+	 * 
+	 * }
+	 */
+	
+	// controller method to read form data and add data to the model
+	@RequestMapping("/processFormVersion3")
+	public String processFormVersion3(
+			@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		// convert the data to all upper case
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey my friend from v3! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+		
+	}
+	
+}
